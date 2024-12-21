14c03cca6fd067f3ac0b43f8e9528b9e384699dc
Srta. Hayashi <129611813+YumiHayashi@users.noreply.github.com>
2024-12-21 13:22:23 -0300

    Update pesquisa.js

--- pesquisa.js
+++ pesquisa.js
@@ -1,3 +1,19 @@
+// Lista de filmes
+    const movies = [
+        "Interstellar",
+        "Rapunzel",
+        "Duna",
+        "Godzilla",
+        "Your Name",
+        "Deadpool & Wolverine",
+        "Cobra Kai",
+        "Round 6",
+        "Samurai dos Olhos Azuis",
+        "Hannibal",
+        "Percy Jackson e os Olimpianos",
+        "Arcane"
+    ];
+
     // Função de pesquisa
     function search() {
         const searchTerm = document.getElementById('searchInput').value.toLowerCase();
